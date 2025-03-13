c64070a008664bb8a22c419519f3448dc381af9f
izaquedmf <izaquedmf@gmail.com>
2025-03-12 22:13:25 -0300

    add eventlistener mouseover to change the square color

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const container = document.querySelector("#container")
 for(let i = 0; i< 256; i++){
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover",() =>{
+        square.style.backgroundColor = "black";
+    });
     container.appendChild(square);
 }
